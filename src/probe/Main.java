a41a82cfdf7646b94fc3606e434a1bafb33d7b07
brat-kuzma <brat.kuzma@gmail.com>
2015-05-20 19:28:09 +0400

    worked method getChange

--- src/probe/Main.java
+++ src/probe/Main.java
@@ -1,32 +1,44 @@
 package probe;
 
 public class Main {
-
-    public static  getChange(int a, int denga) {
-        if (a >= denga) {
-            a = a - denga;
-            return  System.out.println("Your money " + denga +" a =" + a);
-
-    }
-
-    public static void main(String[] args) {
-        int a = 100 - 47;
-        //     53
+    //int a; int denga;
+    //public void setChange(int a);
+    public static void getChange(int a) {
         int[] num = new int[]{10, 2, 1};
         while (a != 0) {
-            for (int denga : num)
-           // for (int i = 0; i <= num.length; i++) {
-                if (getChange(a, denga)) {
-                    break;
+            for (int denga : num) {
+                if (a >= denga) {
+                    a = a - denga;
+                    System.out.println("Your money " + denga + " a =" + a);
+                    //  continue;
                 }
+                break;
+            }
+            //return a;
+        }
+    }
+    public static void main(String[] args) {
+        Main.getChange(55);
+     //   System.out.print("Your money " + denga +" a =" + a);
+    }
+}
+  //     int a = 100 - 47
+  //     53
+  //   int[] num = new int[]{100, 50, 10, 2, 1};
+  //  Main raq = new Main();
+
+           // for (int i = 0; i <= num.length; i++) {
+          //      if (calc.getChange(55)) {
+          //          break;
+           //     }
                 /*
                 if (a >= num[i]) {
                     a = a - num[i];
                     System.out.println("Your money " + num[i] +" a =" + a);
                     break;
                 }
-                */            }
+                           }
 
 
         }
-    }
+    } */
